9b3ebd4ffca52745252e5624bbd0896f44d56c11
𝙽𝚊𝚋𝚒𝚕 𝙺𝚑𝚘𝚗𝚍𝚊𝚔𝚎𝚛 𝙰. <nabilkd@nabilkd.com>
2026-09-21 14:19:19 -0700

    Fix formatting in __init__.py

---
+++ src/neural_ik/robotics/__init__.py
@@ -0,0 +1,6 @@
+"""Robotics subsystem: robot models, kinematics, geometry."""
+
+from neural_ik.robotics.models.planar_2r import Planar2R
+from neural_ik.robotics.models.robot import RobotModel
+
+__all__ = ["Planar2R", "RobotModel"]
